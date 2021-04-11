b6d925984d73fca4771c48c3da1aae2bfe06af42
ronald327 <ronald3217@gmail.com>
2021-04-11 16:14:56 -0600

    add boostrap
    update

--- components/Profile.js
+++ components/Profile.js
@@ -4,7 +4,13 @@ const Profile = ({profile}) => {
     console.log(profile);
     const { name, username, phone, website } = profile
     return ( 
-        <h1>{name} </h1>
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">{name} </h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">{phone}</a>
+            </div>
+        </div>
      );
 }
  
--- pages/About.js
+++ pages/About.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Head from 'next/head'
+import Header from '../components/Header'
 import Profile from '../components/Profile'
 
 const About = ({profile}) => {
     
     return ( 
         <React.Fragment>
-            <h1>Pagina sobre mi</h1>
+            <Head>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
+            </Head>
+            <Header />
+            <h1>Acerca de: {profile.name} </h1>
             <Profile 
                 profile={profile}
             />
