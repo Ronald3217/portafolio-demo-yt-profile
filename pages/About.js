ab0f5f31412f282d3ae4495480cc04fd49c5b718
ronald327 <ronald3217@gmail.com>
2021-04-11 18:09:03 -0600

    javascriptreact
    set javascriptreact snippet to vs code

--- pages/About.js
+++ pages/About.js
@@ -3,32 +3,33 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Profile from '../components/Profile'
 
-const About = ({profile}) => {
-    
-    return ( 
+const About = ({ profile }) => {
+
+    return (
         <React.Fragment>
             <Head>
+                <title>Acerca de {profile.name} </title>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
             </Head>
             <Header />
             <h1>Acerca de: {profile.name} </h1>
-            <Profile 
+            <Profile
                 profile={profile}
             />
         </React.Fragment>
-     );
+    );
 }
- 
+
 export default About;
 
-export async function getStaticProps(){
+export async function getStaticProps() {
 
     const respuesta = await fetch('https://jsonplaceholder.typicode.com/users/1')
     const resultado = await respuesta.json()
 
     return {
         props: {
-            profile:resultado
+            profile: resultado
         }
     }
 }
