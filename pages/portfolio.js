8b51409725163501b99f7370f283fe1560b7badf
ronald327 <ronald3217@gmail.com>
2021-04-14 21:12:29 -0600

    Create portfolio.js

---
+++ pages/portfolio.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+const Portafolio = () => {
+    return ( 
+        <Layout>
+            <h1>Ryan Ray - Projects</h1>
+        </Layout>
+     );
+}
+ 
+export default Portafolio;
